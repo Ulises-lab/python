8db8a373a35aff7d91a917b3a5a403e8afcc4aef
Ulises <44478938+Ulises-lab@users.noreply.github.com>
2020-11-14 22:28:28 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Tipos de variables en python
+variable_int = 1
+variable_float = 7.3
+variable_diccionario = {"nombre":"ulises","apellido":"pokemon"}
+variable_string = "Esto es una cadena"
+variable_list = [1,2,3,4,5]
+variable_chart = "U"
+
+variable_personas = [{"nombre":"Ulises","nombre2":"Ulysses"}]
+                                          |
+                                          '--> Lista de diccionario
+
+# Mostrar variables en salida de terminal
+print(variable_int)
+print(variable_float)
+print(variable_diccionario)
+print(variable_string)
+print(variable_list)
+print(variable_chart)
+
+# Mostrar tipo de dato que tengo en cada variable
+print(type(variable_int))
+print(type(variable_float))
+print(type(variable_string))
+print(type(variable_diccionario))
+print(type(variable_chart))
+print(type(variable_list))
