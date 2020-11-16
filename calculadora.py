e35f5bcd8f4a2c4fd1002da4ef7904137eb5eb19
Ulises <44478938+Ulises-lab@users.noreply.github.com>
2020-11-16 15:29:48 -0600

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+# Creacion de una calculadora para suma, resta, multiplicacion y divicion (dos cantidades)
+
+# Denfinir las variables de entrada en la calculadora (los numeros)
+numero1 = float(input("Dame tu primer numero: "))
+numero2 = float(input("Dame tu segundo numero: "))
+            |
+            '--> Para poder hacer las operaciones con punto (ejemplo, 0.5, 3.2, 000.1, etc)
+
+operador_aritmetico = input("Que deceas hacer con esos numeros (+|-|*|/): ")
+
+if operador aritmetico == "+";
+  suma = numero1 + numero2
+  # Primera forma de inprimir una variable
+  print(suma)
+elif operador_aritmetico == "-";
+  resta = numero1 - numero2
+  # Segunda forma de imprimir una variable
+  print(f"El resultado es: {resta}")
+elif operador_aritmetico == "*";
+  multiplicacion = numero1 * numero2;
+  # Tercer foma de imprimir una variable
+  print("El resultado es: {}".format(multiplicacion))
