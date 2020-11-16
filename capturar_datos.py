8c23662228afc00d784126e485a5f42aa61b8d3a
Ulises <44478938+Ulises-lab@users.noreply.github.com>
2020-11-16 14:55:01 -0600

    Create capturar_datos.py

---
+++ capturar_datos.py
@@ -0,0 +1,7 @@
+# Capturar dato en terminal
+
+variable_edad = input("Dame tu edad: ")
+variable_nombre = "Ulises"
+
+Saludo = "Hola {} tienes {}" .format(variable_nombre,variable_edad)
+print(Saludo)
